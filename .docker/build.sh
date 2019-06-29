4a97a95e14937011e9b50d1d2af105ab82c19d3e
AnyMoe <29625989+AnyMoe@users.noreply.github.com>
2019-06-29 15:34:15 +0800

    unbound version

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 CONFS=(accelerated-domains.china google.china apple.china)
-cd /etc/dnsmasq.d/
-for confName in ${CONFS[@]}
-do
-    if [ ! -f "${confName}.conf" ]; then
-    wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$confName.conf
-      if [ ! -z $CN_DNS ];then
-      sed -i "s|^\(server.*\)/[^/]*$|\1/$CN_DNS|" ./$confName.conf
-      fi
-    fi
-done
-if [ ! -f "bogus-nxdomain.china.conf" ]; then
-  wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/bogus-nxdomain.china.conf
+cd /root/
+if [ ! -f "master.zip" ]; then  
+wget -c https://github.com/felixonmars/dnsmasq-china-list/archive/master.zip
+unzip master.zip && cd dnsmasq-china-list-master
+make SERVER="${CN_DNS1}\n  forward-addr: 1.2.4.8${CN_DNS2}" unbound
+cp -f *.unbound.conf /etc/unbound/conf
+#cp bogus-nxdomain.china.conf /etc/unbound/conf
+
+(
+cat << EOF
+forward-zone:
+  name: "xn--fiqz9s."
+  forward-addr: ${CN_DNS1}
+  forward-addr: ${CN_DNS2}
+
+forward-zone:
+  name: "xn--zfr164b."
+  forward-addr: 1.2.4.8
+EOF
+) >  /etc/unbound/conf/cn.unbound.conf
 fi
--- README.md
+++ README.md
@@ -12,9 +12,11 @@ docker-compose up -d
 enjoy!
 
 P.S. you should install [Docker](https://www.docker.com) and [Docker Compose](https://docs.docker.com/compose/) first.
+PPS. By default, public network access of ipv4 is disabled. If you need external services, change  ``access-control: 0.0.0.0/0 refuse`` to ``access-control: 0.0.0.0/0 allow`` in acl.conf.
 
 ## Environment Variables:
 
 | Name              | Default Value               |
 | ----------------- | --------------------------- |
-| CN_DNS            | 114.114.114.114             |
+| CN_DNS1           | 114.114.114.114             |
+| CN_DNS2           | 119.29.29.29                |
